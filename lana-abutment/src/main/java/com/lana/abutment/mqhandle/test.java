d573617850017e478dc4dc5234cabacbf0c4cdff
liuyulet <96757756+liuyulet@users.noreply.github.com>
2025-06-21 16:45:59 +0800

    fix:设计、思考数据规则解析处理
    调研、计划使用grovy

--- lana-abutment/src/main/java/com/lana/abutment/mqhandle/test.java
+++
@@ -1,9 +0,0 @@
-package com.lana.abutment.mqhandle;
-
-/**
- * @auther liuyulet
- * @date 2024/9/7 22:54
- *
- */
-public class test {
-}
--- lana-abutment/src/main/java/com/lana/abutment/pushtoplatforms/test.java
+++
@@ -1,8 +0,0 @@
-package com.lana.abutment.pushtoplatforms;
-
-/**
- * @auther liuyulet
- * @date 2024/9/7 22:55
- */
-public class test {
-}
